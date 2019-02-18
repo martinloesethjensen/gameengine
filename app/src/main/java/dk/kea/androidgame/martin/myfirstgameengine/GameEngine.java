a3bc78635e8f75c634a756289b62cbcbfcf76fd8
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-02-18 15:01:13 +0100

    First commit

---
+++ app/src/main/java/dk/kea/androidgame/martin/myfirstgameengine/GameEngine.java
@@ -0,0 +1,51 @@
+package dk.kea.androidgame.martin.myfirstgameengine;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public abstract class GameEngine extends AppCompatActivity
+{
+    public abstract Screen createStartScreen();
+
+    public void setScreen(Screen screen)
+    {
+    }
+
+    public Bitmap loadBitmap(String fileName)
+    {
+        return null;
+    }
+
+    public void clearFrameBuffer(int color)
+    {
+
+    }
+
+    public void drawBitmap(Bitmap bitmap, int x, int y)
+    {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+}
